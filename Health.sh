c2daa8e597b7951776bbd4c36693cd171a0efd1c
Davido-sesay55 <106459770+Davido-sesay55@users.noreply.github.com>
2022-05-29 20:25:49 -0400

    Update Health.sh

--- Health.sh
+++ Health.sh
@@ -11,6 +11,6 @@ git commit -m "committed"
 
 for i in "${!map[@]}"
 do
-        git remote add $i https://github.com/Koncept55/${map[$i]}.git
+        git remote add $i https://github.com/David-sesay55/${map[$i]}.git
         git pull -u $i DemoBranch
 done
